81413a12bc10ccae6902ecd8ce4e8d89b944e004
ebelike9 <ebelike4@gmail.com>
2025-02-06 17:28:18 +0100

    setting up stream.io

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,57 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { io } from 'socket.io-client';
 
- const socket = io('http://localhost:8080');
+import './App.css'
+import { StreamChat } from "stream-chat";
+import { ChannelList, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [client, setclient] = useState(null)
+  const [channel, setChannel] = useState(null);
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log(socket.id);
-    });
-    socket.on('disconnect', () => {
-      console.log('disconnected');
-    });
-  }, []);
+    const connect = async () => {
+      const chatclient = StreamChat.getInstance("xs5sfxysfyey");
+      await chatclient.connectUser(
+        {
+          id: "james",
+          name: "will james",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        chatclient.devToken("james"),
+      );
 
+      const channel = chatclient.channel("messaging", "travel", {
+        name: "Awesome channel about traveling",
+        members: ["james"],
+      });
+      await channel.watch();
+
+      setChannel(channel);
+      setclient(chatclient);
+    }
+    connect();
+
+    if (client ) return () => {
+      client.disconnect();
+    }
+  }, [])
+
+  if (!client) {
+    return <div>Loading...</div>
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Chat client={client}>
+      <ChannelList  />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   )
 }
 
